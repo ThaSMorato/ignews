93aef461f58c6db6507a07756c88870723971673
ThaSMorato <tha.s.morato@gmail.com>
2021-09-23 08:20:48 -0300

    reafctot: added provider and sigIn/out functions

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,20 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, useSession, signOut } from "next-auth/client";
 
 import styles from "./styles.module.scss";
 
 export const SignInButton = () => {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
-    <button className={styles.signInButton}>
+  return session ? (
+    <button onClick={() => signOut()} className={styles.signInButton}>
       <FaGithub color='#04d361' />
-      Thales Morato
+      {session.user.name}
       <FiX color='#737380' className={styles.closeIcon} />
     </button>
   ) : (
-    <button className={styles.signInButton}>
+    <button onClick={() => signIn("github")} className={styles.signInButton}>
       <FaGithub color='#eba417' />
       Sing in with Github
     </button>
