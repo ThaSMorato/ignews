93706aad0a8a1b773c583a4f7f19055802a5f271
ThaSMorato <tha.s.morato@gmail.com>
2021-09-28 09:41:49 -0300

    refact: eslint new features next

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @next/next/no-document-import-in-page
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
@@ -5,12 +6,12 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel='shortcut icon' href='favicon.png' type='image/png' />
           <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link
             href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap'
             rel='stylesheet'
           />
-          <link rel='shortcut icon' href='favicon.png' type='image/png' />
         </Head>
         <body>
           <Main />
